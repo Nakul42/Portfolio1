867250d1b5ff3562ad9a3a9e22bd6ecb24249a8c
Nakul Varma <varmanakul42@gmail.com>
2025-10-21 12:15:41 +0530

    First Commit

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Event, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
-  imports: [SidebarComponent, NavbarComponent, RouterOutlet, FooterComponent],
+  imports: [NavbarComponent, RouterOutlet, FooterComponent],
 })
 export class LayoutComponent implements OnInit {
   private mainContent: HTMLElement | null = null;
